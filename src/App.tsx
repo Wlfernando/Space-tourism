9fc1859a048098c571080c5e49787b3f7ff7a36e
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-09-15 22:26:58 -0600

    Feat: implement home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import RootLayout from '@Components/RootLayout'
+import Home from '@Components/Mains/Home/Home'
 
 const router = createBrowserRouter([{
   path: '/',
   element: <RootLayout />,
+  children: [{
+    index: true,
+    element: <Home />
+  }]
 }])
 
 export default function App() {
