452f904c3527ac6a93b8ac512cab4dc81c664680
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-09-20 17:55:07 -0600

    Type: add type for Technology

--- src/utils/const.tsx
+++ src/utils/const.tsx
@@ -2,9 +2,14 @@ import data from './data.json'
 import { formatSrcPhotos } from './utils'
 
 export type PseudoREST = typeof data;
-export type Destinations = PseudoREST['destinations'];
+
+type Destinations = PseudoREST['destinations'];
 export type Place = Destinations[number];
-export type Crew = PseudoREST['crew'];
-export type Member = Crew[number]
+
+type Crew = PseudoREST['crew'];
+export type Member = Crew[number];
+
+type Technologies = PseudoREST['technology'];
+export type Tech = Technologies[number];
 
 export const pseudoREST = formatSrcPhotos(data)
