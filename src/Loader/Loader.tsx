d984574c7ce74ef3463898bb5e3fd333400885e2
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-09-19 20:04:27 -0600

    Feat: add loadSquad function

--- src/Loader/Loader.tsx
+++ src/Loader/Loader.tsx
@@ -1,11 +1,18 @@
 import { Params, redirect } from "react-router-dom";
-import { Place, pseudoREST } from "@utils/const";
-import { capitalize } from "@utils/utils";
-import { pathPlace } from "@utils/routes";
+import { Member, Place, pseudoREST } from "@utils/const";
+import { capitalize, readName } from "@utils/utils";
+import { pathPlace, pathSquad } from "@utils/routes";
 
-export function LoadPlace({ params: { place } }: {params: Params<'place'>}) {
+export function loadPlace({ params: { place } }: {params: Params<'place'>}) {
   if (!place) return redirect(pathPlace)
   const thePlace = capitalize(place);
   const destination = pseudoREST.destinations.find(({ name }) => name === thePlace) as Place;
   return { destination }
 }
+
+export function loadSquad({ params: { squad }}: {params: Params<'squad'>}) {
+  if (!squad) return redirect(pathSquad)
+  const theSquadMember = readName(squad)
+  const member = pseudoREST.crew.find(({name}) => name === theSquadMember) as Member
+  return { member }
+} 
