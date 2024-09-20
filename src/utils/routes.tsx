ca2ab294292219fe7b33e0163d9445603bebc44e
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-09-19 20:04:02 -0600

    Feat: create routes for crew

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -7,12 +7,18 @@ export const linksNav = [
   [`/${technology}`, technology],
 ];
 
-export const [place] = [`${destination}/:place?`]
+export const [place, squad] = [`${destination}/:place?`, `${crew}/:squad?`]
 
 const destinationNames = ['moon', 'mars', 'europa', 'titan'];
 const [moon] = destinationNames;
 
-export const [pathPlace] = [`/${destination}/${moon}`];
+const crewMembers = ['douglas-hurley', 'mark-shuttleworth', 'victor-glover', 'anousheh-ansari']
+const [douglas] = crewMembers
+
+export const [pathPlace, pathSquad] = [`/${destination}/${moon}`, `/${crew}/${douglas}`];
+
+export const linksCrew = crewMembers
+  .map(c => [`/${crew}/${c}`, ''])
 
 export const linksDestination = destinationNames
   .map(d => [`/${destination}/${d}`, d]);
