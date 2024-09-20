aa170d0ca4ebf0300429ca6fb06e49d1ec6b4aa6
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-09-20 17:54:22 -0600

    Feat: add loadTech loader function

--- src/Loader/Loader.tsx
+++ src/Loader/Loader.tsx
@@ -1,18 +1,24 @@
 import { Params, redirect } from "react-router-dom";
-import { Member, Place, pseudoREST } from "@utils/const";
-import { capitalize, readName } from "@utils/utils";
-import { pathPlace, pathSquad } from "@utils/routes";
+import { capitalize, get, readName, readTech } from "@utils/utils";
+import { pathLauch, pathPlace, pathSquad } from "@utils/routes";
 
 export function loadPlace({ params: { place } }: {params: Params<'place'>}) {
   if (!place) return redirect(pathPlace)
   const thePlace = capitalize(place);
-  const destination = pseudoREST.destinations.find(({ name }) => name === thePlace) as Place;
+  const destination = get('destinations', thePlace);
   return { destination }
 }
 
 export function loadSquad({ params: { squad }}: {params: Params<'squad'>}) {
   if (!squad) return redirect(pathSquad)
   const theSquadMember = readName(squad)
-  const member = pseudoREST.crew.find(({name}) => name === theSquadMember) as Member
+  const member = get('crew', theSquadMember);
   return { member }
-} 
+}
+
+export function loadTech({ params: { tech }}: {params: Params<'tech'>}) {
+  if (!tech) return redirect(pathLauch)
+  const techName = readTech(tech)
+  const theTech = get('technology', techName);
+  return { tech: theTech }
+}
