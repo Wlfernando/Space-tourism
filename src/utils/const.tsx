df230a9413061f4f7b203fe38b254c09c5b60ed1
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-09-19 20:02:50 -0600

    Type: create type for Crew

--- src/utils/const.tsx
+++ src/utils/const.tsx
@@ -4,5 +4,7 @@ import { formatSrcPhotos } from './utils'
 export type PseudoREST = typeof data;
 export type Destinations = PseudoREST['destinations'];
 export type Place = Destinations[number];
+export type Crew = PseudoREST['crew'];
+export type Member = Crew[number]
 
 export const pseudoREST = formatSrcPhotos(data)
