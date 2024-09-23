c7802746777ca7026b940be38223d871cece988b
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-09-23 15:03:36 -0600

    Fix: use path again

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
+const resolve = (route: string) => path.resolve(__dirname, route)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@Components': '/src/components',
-      '@Nav': '/src/Components/Nav/Nav.tsx',
-      '@utils': '/src/utils',
+      '@Components': resolve('./src/components'),
+      '@Nav': resolve('./src/Components/Nav/Nav.tsx'),
+      '@utils': resolve('./src/utils'),
     }
   }
 })
