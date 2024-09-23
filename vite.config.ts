681e5881a2eaeb684073e36e56ab25dd077e0a8f
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-09-23 13:11:08 -0600

    fix: resolve ENOENT path related

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@Components': path.resolve(__dirname, './src/components'),
-      '@Nav': path.resolve(__dirname, './src/Components/Nav/Nav.tsx'),
-      '@utils': path.resolve(__dirname, './src/utils'),
+      '@Components': '/src/components',
+      '@Nav': '/src/Components/Nav/Nav.tsx',
+      '@utils': '/src/utils',
     }
   }
 })
